774f6ac642ba335929f6353499bd508945828e8d
Patrick Garcia <patdalcia@gmail.com>
2022-07-07 18:37:18 -0700

    Finished Milestone 5

---
+++ Milestone5/SourceCode/Organized/Organized/addCourse.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Organized
+{
+    /// <summary>
+    /// Interaction logic for addCourse.xaml
+    /// </summary>
+    public partial class addCourse : Window
+    {
+        private businessService service;
+        private CourseViewModel courseViewModel;
+
+        public addCourse()
+        {
+            courseViewModel = new CourseViewModel();
+            DataContext = courseViewModel;
+
+            InitializeComponent();
+            service = new businessService();
+        }
+
+        /* Click listener, triggers when add course button is clicked */
+        private void createCourse(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                List<Assignment> aList = new List<Assignment>();
+
+                courseViewModel.Name = name.Text;
+
+                Course course = new Course()
+                {
+                    name = name.Text,
+                    description = description.Text,
+                    professor = professor.Text,
+                    start_date = String.Format("{0:dd MM yyyy}", startDate.Text),
+                    end_date = String.Format("{0:dd MM yyyy}", endDate.Text),
+                    assignments = aList
+                };
+
+                if(service.addCourse(course))
+                {
+                    this.Close();
+                }
+                else
+                {
+                    throw new Exception("ERROR: Could not add Course");
+                }
+            }
+            catch(Exception exception)
+            {
+                Trace.WriteLine(exception.ToString());
+            }
+        }
+    }
+}
