8db1f3da842584a2d6df107ba3af738c30746154
patdalcia <49659510+patdalcia@users.noreply.github.com>
2022-07-10 22:49:34 -0700

    Final Commit

---
+++ Milestone7/SourceCode/Organized/Organized/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Organized
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            SplashScreen2 splash = new SplashScreen2("Images/SplashScreenImage.png");
+            splash.Show(TimeSpan.FromSeconds(3));//Show for at least 2 seconds min duration
+        }
+    }
+}
